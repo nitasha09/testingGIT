2db1ed3949e18cc7e424bafee660e270a5754dd9
Nitasha Martin <shukshara09@gmail.com>
2021-04-17 15:45:10 +0800

    Add project files.

---
+++ testingGIT/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim strKelas(5, 3) As String
+        strKelas(0, 0) = "DDT1A"
+        strKelas(0, 1) = "DDT1B"
+        strKelas(1, 0) = "DDT2A"
+        strKelas(1, 1) = "DDT2B"
+        strKelas(1, 2) = "DDT2C"
+
+    End Sub
+End Class
---
+++ testingGIT/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("testingGIT")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("testingGIT")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6ff3be59-b21c-447c-99ce-9fd1b6b6c87f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
